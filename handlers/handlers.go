e36a8d7b1e8ff8562825a766aa36911ad7a769f6
Syun199986 <syun990806@gmail.com>
2024-07-02 00:18:19 +0900

    gorilla/muxの導入、パスパラメータ受け取り処理追記

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,57 +1,40 @@
 package handlers
 
-import(
+import (
+	"fmt"
 	"io"
 	"net/http"
-	"fmt"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
-	func HelloHandler(w http.ResponseWriter, req *http.Request) {
-		if req.Method == http.MethodGet {
-			io.WriteString(w, "Hello world\n")
-		} else {
-			http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
-		}
-	}
-
-	func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-		if req.Method == http.MethodPost {
-			io.WriteString(w, "Posting Article...\n")
-		} else {
-			http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
-		}
-	}
-
-	func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
-		if req.Method == http.MethodGet {
-		io.WriteString(w, "Article List\n")
-	} else {
-		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
-	}
+func HelloHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Hello, world\n")
 }
 
-	func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
-		articleID := 1
-		resString := fmt.Sprintf("Article No.%d\n", articleID)
-		if req.Method == http.MethodGet {
-		io.WriteString(w, resString)
-		} else {
-			http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
-		}
-	}
+func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Posting Article...\n")
+}
 
-	func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
-		if req.Method == http.MethodPost {
-		io.WriteString(w, "Posting Nice...\n")
-		} else {
-			http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
-		}
-	}
+func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Article List\n")
+}
 
-	func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
-		if req.Method == http.MethodPost {
-		io.WriteString(w, "Posting Comment...\n")
-		} else {
-			http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
-		}
+func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
+	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
+	if err != nil {
+		http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+		return
 	}
+	resString := fmt.Sprintf("Article No.%d\n", articleID)
+	io.WriteString(w, resString)
+}
+
+func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Posting Nice...\n")
+}
+
+func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Posting Comment...\n")
+}
